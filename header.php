ce576c7c7d591b152d523b55209e5e6ecae9ab28
tanawat <tanawat_klomklom@cmu.ac.th>
2018-03-29 16:34:22 +0700

    homepage

---
+++ header.php
@@ -0,0 +1,42 @@
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="css/css_new.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <title>Marathon</title>
+  </head>
+  <body>
+    <!-- navbar -->
+    <nav class="navbar navbar-inverse bg-1">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <img src="picture/logo.jpg" alt="Logo" height="52" width="150">
+                
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li class=""><a href="2.php">Store</a></li>
+                    <li class=""><a href="#.php">picture</a></li>
+                    <li class=""><a href="#.php">video</a></li>
+                    <li class=""><a href="#">Event All</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- <li class=""><a href="register.php"><span class="glyphicon glyphicon-user"></span> Register Now</a></li> -->
+                    <!-- <li class="active"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
+                    <button class="btn btn-danger navbar-btn"><a href="register1.php"><span class="glyphicon glyphicon-log-in"></span> Register Now</a></button>
+                    <button class="btn btn-success navbar-btn"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></button>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</html>
---
+++ homepage.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<?php require_once("header.php"); ?>
+    <!-- slide -->
+    <div class="container bg-2">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+        
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active " > 
+                    <img src="picture/img1.jpg" style="width:100%;" class="center">
+                    <button onclick="location.href='register1.php'" class="btn" >REGISTRATION IS NOW OPEN</button>
+                </div>
+        
+                <div class="item" >
+                    <img src="picture/img2.jpg" style="width:100%;"  class="center">
+                    <button onclick="location.href='space.html'"class="btn">REGISTRATION IS NOW OPEN</button>
+                </div>
+                
+                <div class="item">
+                    <img src="picture/img3.jpg" style="width:100%;"  class="center" >
+                    <button onclick="location.href='space.html'" class="btn">REGISTRATION IS NOW OPEN</button>
+                </div>
+            </div>
+        
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+    <footer class="container-fluid bg-4 text-center">
+        <p>Bootstrap Theme Made By <a href="https://www.w3schools.com">www.w3schools.com</a></p> 
+    </footer>
+  </body>
+</html>
