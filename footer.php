dba7192a5a2e0082777e051366330376acfbd263
apboomap <apboomap@outlook.com>
2018-03-31 20:16:23 +0700

    First Work

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+<link href="css/css_new.css" rel="stylesheet">
+
 <footer class="container-fluid bg-4 text-center">
         <p>Bootstrap Theme Made By <a href="https://www.w3schools.com">www.w3schools.com</a></p> 
     </footer>
