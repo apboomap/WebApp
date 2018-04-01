29ff011258f1ed0be0e9130423e114163fbe99c4
apboomap <apboomap@outlook.com>
2018-04-01 22:14:35 +0700

    Aof's work

---
+++ eventAll.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+        <title>Events</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- css file -->   
+        <link href="css/eventAll.css" rel="stylesheet" type="text/css">
+        
+    
+ 
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-inverse bg-1">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <img src="picture/logo.jpg" alt="Logo" height="52" width="150">
+                
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class=""><a href="homepage.php">Home</a></li>
+                    <li class=""><a href="register2.php">Store</a></li>
+                    <li class=""><a href="model.php">picture</a></li>
+                    <li class=""><a href="#.php">video</a></li>
+                    <li class=""><a href="#">Event All</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- <li class=""><a href="register.php"><span class="glyphicon glyphicon-user"></span> Register Now</a></li> -->
+                    <!-- <li class="active"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
+                    <button class="btn btn-danger navbar-btn"><a href="register1.php"><span class="glyphicon glyphicon-log-in"></span> Register Now</a></button>
+                    <button class="btn btn-success navbar-btn"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></button>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+
+<br>
+<center>
+<div class="container">
+                <div class="row">
+                        <p class="lead"><h3>ค้นหางานวิ่งจากเดือน หรือ ปี</h3></p>    <br>                   
+                                    <!-- Our Special dropdown has class show-on-hover -->
+                                    <div class="btn-group show-on-hover">
+                                    <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">
+                                        เดือน <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">มกราคม</a></li>
+                                        <li><a href="#">กุมภาพันธ์</a></li>
+                                        <li><a href="#">มีนาคม</a></li>               
+                                        <li><a href="#">เมษายน</a></li>
+                                        <li><a href="2018may.php" target="_blank">พฤษภาคม</a></li>
+                                        <li><a href="#">มิถุนายน</a></li>
+                                        <li><a href="2018jul.php" target="_blank">กรกฎาคม</a></li>
+                                        <li><a href="#">สิงหาคม</a></li>
+                                        <li><a href="#">กันยายน</a></li>
+                                        <li><a href="#">ตุลาคม</a></li>
+                                        <li><a href="#">พฤศจิกายน</a></li>
+                                        <li><a href="#">ธันวาคม</a></li>
+									</ul>
+									</div>
+													                  
+                                    <!-- Our Special dropdown has class show-on-hover -->
+                                    <div class="btn-group show-on-hover">
+                                    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown">
+                                        ปี <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu" role="menu">
+										<li><a href="#">2015</a></li>
+										<li><a href="#">2016</a></li>
+                                        <li><a href="2017dec.php" target="_blank">2017</a></li>
+                                        <li><a href="#">2018</a></li>
+                                        <li><a href="#">2019</a></li>               
+                                        <li><a href="#">2020</a></li>
+									</ul>
+                                    </div>
+                </div>
+        </div>
+</center>
+
+<br><br>
+<!--event-->
+<div class="container">
+            <div class="w3-col">
+                <div class="row">                
+                    <ul class="event-list">
+                        <!--cmu-->                                                
+                            <li>
+                                <time datetime="2018-02-11">
+                                    <span class="day">11</span>
+                                    <span class="month">ก.พ.</span>
+                                    <span class="year">2018</span>
+                                    <span class="time">All Day</span>
+                                </time>
+                                <img alt="CMU" src="event/CMU/even.jpg"/>
+                                <div class="info">
+                                    <h2 class="title">CMU Marathon 2018</h2>
+                                    <p class="desc"><span class="glyphicon glyphicon-map-marker"></span><a href="https://goo.gl/maps/vcMkD2jsjgo">  Chiang Mai University </a></p>
+                                    <ul>
+                                        <li style="width:25%;"><span class="fa fa-male"></span> [42.195L]</li>
+                                        <li style="width:25%;"><span class="fa fa-male"></span> [21.1K]</li>
+                                        <li style="width:25%;"><span class="fa fa-male"></span> [10.5K]</li>
+                                        <li style="width:25%;"><span class="fa fa-child"></span> [4.6K]</li>
+                                    </ul>
+                                </div>
+                                <div class="detail">
+                                    <ul>
+                                        <li class="facebook" style="width:33%;"><a href="#facebook"> ข้อมูลเพิ่มเติม </a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            
+                            <!--หาดคุ้งวิมาน-->
+                            <li>
+                                <time datetime="2018-04-01">
+                                    <span class="day">1</span>
+                                    <span class="month">เม.ย.</span>
+                                    <span class="year">2018</span>
+                                    <span class="time">All Day</span>
+                                </time>
+                                <img alt="RunToSea" src="event/RunToSea/even1.jpg" />
+                                <div class="info">
+                                    <h2 class="title">RUN TO SEA 2018</h2>
+                                    <p class="desc"><span class="glyphicon glyphicon-map-marker"></span><a href="https://goo.gl/maps/pWAKPDXvLgQ2">  หาดคุ้งวิมานเนินนางพญา</a></p>
+                                    <ul>
+                                        <li style="width:33%;"><span class="fa fa-male"></span> [21K] </li>
+                                        <li style="width:34%;"><span class="fa fa-male"></span> [10K] </li>
+                                        <li style="width:33%;"><span class="fa fa-child"></span> [5K] </li>
+                                    </ul>
+                                </div>
+                                <div class="detail">
+                                    <ul>
+                                        <li class="facebook" style="width:33%;"><a href="#facebook"> ข้อมูลเพิ่มเติม </a></li>
+                                    </ul>
+                                </div>
+                            </li>  
+
+                            <!--วิ่งอมยิ้ม-->
+                            <li>
+                                <time datetime="2018-04-01">
+                                    <span class="day">1</span>
+                                    <span class="month">เม.ย.</span>
+                                    <span class="year">2018</span>
+                                    <span class="time">All Day</span>
+                                </time>
+                                <img alt="VingAomYim" src="event/VingAomYem/even2.jpg" />
+                                <div class="info">
+                                    <h2 class="title">วิ่งอมยิ้ม 2018</h2>
+                                    <p class="desc"><span class="glyphicon glyphicon-map-marker"></span><a href="https://goo.gl/maps/phYSvyiYcCC2">  ณ ตลาดอมยิ้ม อำเภอจอมบึง</a></p>
+                                    <ul>
+                                        <li style="width:33%;"><span class="fa fa-male"></span> [21K] </li>
+                                        <li style="width:34%;"><span class="fa fa-male"></span> [10.5K] </li>
+                                        <li style="width:33%;"><span class="fa fa-child"></span> [5K] </li>
+                                    </ul>
+                                </div>
+                                <div class="detail">
+                                    <ul>
+                                        <li class="facebook" style="width:33%;"><a href="#facebook"> ข้อมูลเพิ่มเติม </a></li>
+                                    </ul>
+                                </div>
+                            </li>                        
+                    </ul> 
+                </div>          
+            </div>
+        </div>
+
+</body>
+</html>
+
+
+
+
+    
+
+
+
