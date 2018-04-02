6d5b4bea03266e04030291e74fe878677342e48a
apboomap <apboomap@outlook.com>
2018-04-03 01:55:22 +0700

    work' A

--- register2.5.1.php
+++ register2.5.1.php
@@ -10,7 +10,7 @@
 
 
  <!-- process step bar -->
- <div class="container">
+<div class="container">
         <ul class="progressbar">
             <li class="active">ลงทะเบียน</li>
             <li class="active">ตรวจสอบ</li>
@@ -18,41 +18,35 @@
             <li>ชำระเงิน</li>
             <li>เสร็จสิ้น</li>
         </ul>
-    </div>
+</div>
 
     
 <div class="container">
-        <div class="container">
-            
-        <header class="clearfix">
-                <div id="logo">
+    <header class="clearfix">
+        <div id="logo">
             <h3>นักวิ่งคนที่1</h3>
             <a href="register1.php"><span></span></a>
-            </div>
+        </div>
             <button type="button" class="close" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+            <span aria-hidden="true">&times;</span>
             </button>
-        </header>
-
-        <header class="clearfix">
-                <div id="logo">
+    </header>
+    <header class="clearfix">
+        <div id="logo">
             <h3>+เพิ่มเพื่อน</h3>
             <a onclick ="document.getElementById('id01').style.display='block'"><span></span></a>
-            </div>
-            <button type="button" class="close" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-            </button>
-        </header>
-        
         </div>
+            <button type="button" class="close" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+    </header>
+</div>
         
+    <div class="container">
+        <button onclick="document.getElementById('id02').style.display='block'" class="w3-button w3-red">กรอกแบบสอบถาม</button><tab></tab>	
+        <button onclick="location.href='register2.5.2.php'" class="w3-button w3-blue w3-right" >ต่อไป</button>
+    </div>  
 
-    <div class="carousel-inner">
-            <div class="item active container" > 
-                <button onclick ="document.getElementById('id02').style.display='block'" class="btnnn">กรอกแบบสอบถาม</button>	
-                <button onclick="location.href='register2.5.2.php'" class="btnn" >ต่อไป</button>
-            </div>
-    </div>
 <div class="w3-container">
     <div id="id01" class="w3-modal">
         <div class="w3-modal-content">
@@ -741,14 +735,99 @@
                 </div>
             </div>
         
+<<<<<<< HEAD
                     <div class="carousel-inner">
+=======
+            <div class="form-group">
+                <div class="form-group">
+                    <label for="disease" class="col-sm-3 control-label">โรคประจำตัว</label>
+                    <div class="col-sm-6">
+                        <input type="text" name="disease" placeholder="" class="form-control" autofocus>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="allergy" class="col-sm-3 control-label">การแพ้ยา</label>
+                    <div class="col-sm-6">
+                        <input type="text" name="allergy" placeholder="" class="form-control" autofocus>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="regular drugs" class="col-sm-3 control-label">ยาที่ใช้ประจำ</label>
+                    <div class="col-sm-6">
+                        <input type="text" name="regular drugs" placeholder="" class="form-control" autofocus>
+                    </div>
+                </div>
+                <label for="blood" class="col-sm-3 control-label">กรุ๊ปเลือด</label>
+                <div class="col-sm-3">
+                    <select name="blood" class="form-control">
+                        <option value="">--BLOOD--</option> 
+                        <option>O</option>
+                        <option>A</option>
+                        <option>B</option>
+                        <option>AB</option>
+                    </select>
+                </div>
+            </div>
+             
+
+            <!-- /.form-group -->
+            <div class="form-group">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <br><br><button type="submit" class="btn btn-primary btn-block"><a href="payment.php">บันทึก</a></button>
+                </div>
+            </div>
+        </form>
+    </div> <!-- ./container -->
+            </div>
+        </div>
+    </div>
+    </div>
+    <div id="id02" class="w3-modal">
+        <div class="w3-modal-content">
+            <div class="w3-container">
+                    <span onclick="document.getElementById('id02').style.display='none'" class="w3-button w3-display-topright">&times;</span>
+                    <h1>แบบสอบถาม</h1>
+           
+                    <div class="form-group col-md-12">
+                        <label for="inputCity">1.ทราบ event จากช่องทางไหน</label>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label for="primary" class="btn btn-primary">Facebook <input type="checkbox" id="primary" ></label>
+                        <label for="success" class="btn btn-success">Line <input type="checkbox" id="success" ></label>
+                        <label for="warning" class="btn btn-warning">Friend <input type="checkbox" id="warning" ></label>
+                    </div>
+
+                    <div class="form-group col-md-12">
+                        <label for="inputCity">2.รู้จักเว็บไซต์เราได้อย่างไร</label>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label class="btn btn-primary">Facebook <input type="checkbox" ></label>
+                        <label class="btn btn-success">Line <input type="checkbox" ></label>
+                        <label class="btn btn-warning">Friend <input type="checkbox" ></label>
+                    </div> 
+                  
+                    
+              
+                    <div class="form-group col-md-12">
+                        <label for="inputCity">3.เดินทางมาด้วยวิธีไหน</label>
+                    </div>
+                    <div class="form-group col-md-12">
+                        <label class="btn btn-primary">รถจักรยานยนต์ <input type="checkbox" ></label>
+                        <label class="btn btn-success">รถยนต์ <input type="checkbox" ></label>
+                        <label class="btn btn-warning">มากับเพื่อน <input type="checkbox" ></label>
+                    </div> 
+
+                    <div class="form-group col-md-12">             
+>>>>>>> 809963fdda646acff839c1e77db144c064cc81b4
                         <div class="item active container" > 	
-                            <center><button type="submit" class="pume" ><a href="register2.5.1.php">บันทึก</a></button></center>
+                        <button type="submit" class="pume" ><a href="register2.5.1.php">บันทึก</a></button>
                         </div>
-                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
 
 <?php require_once("footer.php"); ?>   
