e6f718137f3ef15be356c0026f76aba7283b1c1d
thanaphom <pamdhumawong@gmail.com>
2018-04-02 17:03:57 +0700

    modefine runner bill add  generation_compete

--- insert/runners_bills.php
+++ insert/runners_bills.php
@@ -11,8 +11,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 $conn->set_charset("utf8");
-$sql = "INSERT INTO runners_bills(bill_id, run_id, flag_full, flag_half , flag_mini, flag_fun)
-VALUES (1 , 1, 1, 0, 0 ,0),(1, 2, 1, 0, 0 ,0),(2 , 3, 0, 1, 0, 0)";
+$sql = "INSERT INTO runners_bills(bill_id, run_id, flag_full, flag_half , flag_mini, flag_fun, generation_compete)
+VALUES (1 , 1, 1, 0, 0 , 0, '19-26'),(1, 2, 1, 0, 0 , 0, '27-36'),(2 , 3, 0, 1, 0, 0 , '37-46')";
 
 
 if ($conn->multi_query($sql) === TRUE) {
